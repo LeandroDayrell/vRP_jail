31259b66c1e82de28dee747b30e87f568bebd260
Leandro Dayrell Silva <leandrodayrell@live.com>
2017-09-27 14:03:55 -0300

    1

--- vrp_jail/client.lua
+++
@@ -1,9 +0,0 @@
-vRP = Proxy.getInterface("vRP")
-
-AddEventHandler("playerSpawned",function() -- delay state recording
-  
-  Citizen.CreateThread(function()
-    Citizen.Wait(30000)
-    vRP.setHandcuffed({false})
-  end)
-end)
