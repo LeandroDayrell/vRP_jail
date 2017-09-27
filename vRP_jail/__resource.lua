ed6f54869916f90da91509bdb3ea72ea70fe2e1e
Leandro Dayrell Silva <leandrodayrell@live.com>
2017-09-27 14:04:54 -0300

    att

---
+++ vRP_jail/__resource.lua
@@ -0,0 +1,15 @@
+description "vrp_jail"
+--ui_page "ui/index.html"
+
+dependency "vrp"
+
+client_scripts{ 
+  "client/Tunnel.lua",
+  "client/Proxy.lua",
+  "client.lua"
+}
+
+server_scripts{ 
+  "@vrp/lib/utils.lua",
+  "server.lua"
+}
